1a79ff6b39c78d99660d7bad62779940c83c2c08
Jacob Erholt <56915193+JHErholt@users.noreply.github.com>
2024-03-04 10:38:52 +0100

    initialize the things outside of src

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const { DateTime } = require('luxon');
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addWatchTarget('./src/scss/')
+
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/json');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter('postDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
+    eleventyConfig.addFilter('slugify', function(value) {
+        return value;
+    });
+
+    return{
+        dir: {
+            input: 'src',
+            output: 'public'
+        }
+    };
+}
